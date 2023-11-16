229562abbfae161fe026ebfb3dd5735f7cfc62fd
ritankar pal <ritankar0425@gmail.com>
2023-11-16 00:26:05 -0500

    Connection Pooling

---
+++ src/in/ineuron/Test/InsertOP.java
@@ -0,0 +1,60 @@
+package in.ineuron.Test;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import in.ineuron.model.Student;
+import in.ineuron.util.HibernateUtil;
+
+
+public class InsertOP {
+
+	public static void main(String[] args) {
+		
+		Session session = null; 
+		Transaction transaction = null;
+		boolean flag = false;
+		
+		try {
+			session = HibernateUtil.getSession();
+			
+			if(session != null) {
+				 transaction = session.beginTransaction();
+			}
+			
+			if(transaction != null) {
+				
+				Student student = new Student();
+				student.setSname("Sribhumi");
+				student.setSaddress("Bangalore");
+				student.setSage(24);
+				
+				session.save(student);
+				flag = true;
+				
+			}
+			
+		}
+		catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			
+			if(flag) {
+				transaction.commit();
+				System.out.println("Object updated to the database...");
+			}
+			else {
+				transaction.rollback();
+				System.out.println("Object failed to update...");
+			}
+			
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+		
+	}
+
+}
